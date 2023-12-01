5c711492f72a5c500a6d8c2285b0b4d28c1e5f4c
Jancauskas <janc_vy@eoc-001470l.intra.dlr.de>
2023-12-01 09:28:50 +0100

    solution for day1

--- src/aoc2023/day1.py
+++ src/aoc2023/day1.py
@@ -1,7 +1,18 @@
 import click
 
 def parse(inputs):
-    return []
+    result = []
+    for line in inputs:
+        for c in line:
+            if c.isdigit():
+                a = int(c)
+                break
+        for c in line[::-1]:
+            if c.isdigit():
+                b = int(c)
+                break
+        result.append(a * 10 + b)
+    return result
 
 if __name__ == '__main__':
     pass
