5a6e84082129804bba6bb478099a17dad626a0fc
Jancauskas <janc_vy@eoc-001470l.intra.dlr.de>
2023-12-01 09:53:00 +0100

    solved puzzle 2

--- src/aoc2023/day1.py
+++ src/aoc2023/day1.py
@@ -15,7 +15,33 @@ def parse(inputs):
     return result
 
 def parse_adv(inputs):
-    return []
+    spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    result = []
+    for line in inputs:
+        for i, c in enumerate(line):
+            if c.isdigit():
+                a = int(c)
+                break
+            found = False
+            for j, sp in enumerate(spelled):
+                if line[i:].startswith(sp):
+                    a = j + 1
+                    found = True
+            if found:
+                break
+        for i, c in enumerate(line[::-1]):
+            if c.isdigit():
+                b = int(c)
+                break
+            found = False
+            for j, sp in enumerate(spelled):
+                if line[::-1][i:].startswith(sp[::-1]):
+                    b = j + 1
+                    found = True
+            if found:
+                break
+        result.append(a * 10 + b)
+    return result
 
 @click.command()
 @click.option('-i', '--input', help='Input file')
--- test/test_day1.py
+++ test/test_day1.py
@@ -4,5 +4,6 @@ def test_day1():
     inputs = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
     result = parse(inputs)
     assert(result == [12, 38, 15, 77])
+    inputs = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
     result_adv = parse_adv(inputs)
     assert(result_adv == [29, 83, 13, 24, 42, 14, 76])
