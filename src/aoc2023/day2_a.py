c439059e1c9bdc9d9662493bf54b91a59d0acd66
Jancauskas <janc_vy@eoc-001470l.intra.dlr.de>
2023-12-02 09:37:13 +0100

    implemented parse_game

--- src/aoc2023/day2_a.py
+++ src/aoc2023/day2_a.py
@@ -1,4 +1,27 @@
 import click
 
+def parse_game(line):
+    game_id, game_input = line.split(':')
+    game_id = int(game_id.strip().split(' ')[1])
+    games = game_input.strip().split(';')
+    result = []
+    for game in games:
+        game = game.strip()
+        colors = game.split(',')
+        color_counts = [0, 0, 0]
+        for color in colors:
+            color = color.strip()
+            if color.endswith('red'):
+                color_counts[0] += int(color.split(' ')[0].strip())
+            elif color.endswith('green'):
+                color_counts[1] += int(color.split(' ')[0].strip())
+            elif color.endswith('blue'):
+                color_counts[2] += int(color.split(' ')[0].strip())
+            else:
+                raise RuntimeError("Invalid input!")
+        result.append(color_counts)
+    return result
+    
+
 if __name__ == '__main__':
     pass
--- test/test_day2.py
+++ test/test_day2.py
@@ -3,15 +3,15 @@ from aoc2023.day2_a import parse_game
 INPUT = [
     "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
     "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
-    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
     "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
 ]
 
 def test_day2():
-    result = parse_game(INPUT)
+    result = [parse_game(input) for input in INPUT]
     assert(result[0] == [[4, 0, 3], [1, 2, 6], [0, 2, 0]])
-    assert(result[1] == [[0, 2, 1], [1, 3, 4], [1, 1, 1]])
+    assert(result[1] == [[0, 2, 1], [1, 3, 4], [0, 1, 1]])
     assert(result[2] == [[20, 8, 6], [4, 13, 5], [1, 5, 0]])
     assert(result[3] == [[3, 1, 6], [6, 3, 0], [14, 3, 15]])
     assert(result[4] == [[6, 3, 1], [1, 2, 2]])
