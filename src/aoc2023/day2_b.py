6c36325b2577c50caf3f199bae9044ad9afc129b
Jancauskas <janc_vy@eoc-001470l.intra.dlr.de>
2023-12-02 10:03:08 +0100

    finished day2

--- src/aoc2023/day2_b.py
+++ src/aoc2023/day2_b.py
@@ -2,18 +2,23 @@ import click
 from aoc2023.day2_a import parse_game
 
 def check_validity(game):
-    pass
+    return [max([cubes[0] for cubes in game]),
+            max([cubes[1] for cubes in game]),
+            max([cubes[2] for cubes in game])]
     
 @click.command()
 @click.option('-i', '--input', help='Input file')
 def main(input):
-    id_sum = 0
+    result = 0
     with open(input, 'r') as fd:
         for i, line in enumerate(fd.readlines()):
             parsed = parse_game(line)
-            if all([r <= 12 and g <= 13 and b <= 14 for r, g, b in parsed]):
-                id_sum += (i + 1)
-    print(id_sum)
+            minimum_set = check_validity(parsed)
+            power = minimum_set[0] * minimum_set[1] * minimum_set[2]
+            result += power
+            #if all([r <= 12 and g <= 13 and b <= 14 for r, g, b in parsed]):
+            #    id_sum += (i + 1)
+    print(result)
 
 if __name__ == '__main__':
     main()
